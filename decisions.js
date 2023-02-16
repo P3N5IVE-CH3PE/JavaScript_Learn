620cb73f4774083181eebac39cf522a84c8358ae
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2023-02-16 11:35:13 -0800

    new files

---
+++ decisions.js
@@ -0,0 +1,16 @@
+let isClient = true;
+
+function check_status(isClient){
+
+    console.log("Allowed");
+    
+
+
+}else{
+
+    console.log("Not allowed");
+
+
+
+    } 
+
---
+++ variables.js
@@ -0,0 +1,10 @@
+let name = "Bob Dough";
+
+let age;
+age = 36;
+
+let friends = ["Brandon","Marc"];
+
+console.log(freinds.lenght);
+console.log(friends[1];
+
